8d9ab6ebc2622ed94a2e2deea816c20eb90f7c09
yn55m6 <yn55m6@gmail.com>
2026-03-05 00:09:39 +0900

    refactor: 초기화 버튼을 설정 탭으로 이동

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'attendance-app-cache-v59'; // 버전을 업데이트하여 캐시를 갱신할 수 있습니다.
+const CACHE_NAME = 'attendance-app-cache-v60'; // 버전을 업데이트하여 캐시를 갱신할 수 있습니다.
 const urlsToCache = [
     './',
     './index.html',
