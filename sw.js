76ec2a2ff38252d534a9466ec8c23d9730583bf3
yn55m6 <yn55m6@gmail.com>
2026-03-04 21:12:28 +0900

    fix: 네비게이션 탭 전환 로직 안정성 강화

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'attendance-app-cache-v39'; // 버전을 업데이트하여 캐시를 갱신할 수 있습니다.
+const CACHE_NAME = 'attendance-app-cache-v40'; // 버전을 업데이트하여 캐시를 갱신할 수 있습니다.
 const urlsToCache = [
     './',
     './index.html',
